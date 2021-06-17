b3150636182994f8a4a39c82a08d7d807f98931e
widjuu <4yvak08@mail.ru>
2021-06-18 00:32:09 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import XLSX from "xlsx";
+import { Button, Upload, message } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 
-function App() {
+import "antd/dist/antd.css";
+
+export const App = () => {
+  const [file, setFile] = useState([]);
+
+  const props = {
+    onRemove: (file) => {
+      const index = file.indexOf(file);
+      const newFileList = file.slice();
+      newFileList.splice(index, 1);
+      setFile([newFileList]);
+    },
+
+    beforeUpload: (file) => {
+      const isCsvOrXlsx =
+        file.type === "text/csv" ||
+        file.type ===
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+      if (!isCsvOrXlsx) {
+        message.error("Error");
+      }
+
+      if (isCsvOrXlsx) {
+        setFile([file]);
+      }
+    },
+  };
+
+  console.log(file);
+
+  const reader = new FileReader();
+  const rABS = !!reader.readAsBinaryString;
+  reader.onload = (e) => {
+    /* Parse data */
+    const bstr = e.target.result;
+    const wb = XLSX.read(bstr, { type: rABS ? "binary" : "array" });
+    /* Get first worksheet */
+    const wsname = wb.SheetNames[0];
+    const ws = wb.Sheets[wsname];
+    /* Convert array of arrays */
+    const data = XLSX.utils.sheet_to_json(ws, {
+      header: 1,
+    });
+
+    console.log(data);
+  };
+
+  if (file[0]) {
+    reader.readAsBinaryString(file[0]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Upload {...props} fileList={file}>
+        <Button icon={<UploadOutlined />}>Upload</Button>
+      </Upload>
     </div>
   );
-}
+};
 
 export default App;
