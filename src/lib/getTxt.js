1e556bb030787ff8129c520cea1a181ec281da45
widjuu <4yvak08@mail.ru>
2021-12-01 20:31:14 +0300

    добавляем штрих код

--- src/lib/getTxt.js
+++ src/lib/getTxt.js
@@ -5,16 +5,18 @@ export const getTxt = (data) => {
     return null;
   }
 
-  data.map((item) => {
-    txt.push(`${item[0].toString().padStart(4, "0")}.0001="${item[1]}"`);
-    txt.push(`${item[0].toString().padStart(4, "0")}.0002="${item[2]}"`);
-    txt.push(`${item[0].toString().padStart(4, "0")}.0003="1"`);
-    txt.push(`${item[0].toString().padStart(4, "0")}.0004="0"`);
-    txt.push(`${item[0].toString().padStart(4, "0")}.0005="0,000"`);
-    txt.push(`${item[0].toString().padStart(4, "0")}.0006="0,000"`);
+  data
+    .filter((i) => i[0])
+    .map((item) => {
+      txt.push(`${item[0].toString().padStart(4, "0")}.0001="${item[1]}"`);
+      txt.push(`${item[0].toString().padStart(4, "0")}.0002="${item[2]}"`);
+      txt.push(`${item[0].toString().padStart(4, "0")}.0003="1"`);
+      txt.push(`${item[0].toString().padStart(4, "0")}.0004="${item[3] ?? 0}"`);
+      txt.push(`${item[0].toString().padStart(4, "0")}.0005="0,000"`);
+      txt.push(`${item[0].toString().padStart(4, "0")}.0006="0,000"`);
 
-    return null;
-  });
+      return null;
+    });
 
   return ["[1]", ...txt];
 };
