254389ab245fe424b2acbde570a7b177fefb2127
widjuu <4yvak08@mail.ru>
2021-12-09 22:00:28 +0300

    update script

--- src/lib/download.js
+++ src/lib/download.js
@@ -3,15 +3,15 @@ import { encodeCP1251 } from "./encode";
 export const download = (txt) => {
   let xlsxURL = window.URL.createObjectURL(
     new Blob([[txt.join("\r\n")]], {
-      type: "text/plain",
+      type: "text/x-csrc",
     })
   );
   let tempLink = document.createElement("a");
   tempLink.href = xlsxURL;
   tempLink.setAttribute(
     "href",
-    "data:text/plain;charset=cp1251," + encodeCP1251(txt.join("\n"))
+    "data:text/x-csrc;charset=cp1251," + encodeCP1251(txt.join("\n"))
   );
-  tempLink.setAttribute("download", "elves.txt");
+  tempLink.setAttribute("download", "elves.csv2");
   tempLink.click();
 };
--- src/lib/getTxt.js
+++ src/lib/getTxt.js
@@ -5,18 +5,64 @@ export const getTxt = (data) => {
     return null;
   }
 
+  const regExp = /[^a-zа-яё0-9,.\s]/gi;
+
+  const getCode = (code) => {
+    if (code && code.length > 0 && code.length <= 13) {
+      return code;
+    }
+
+    if (code && code.length > 13) {
+      return code.substring(0, 13);
+    }
+
+    return 0;
+  };
+
+  const getName = (name) => {
+    return name?.substring(0, 27).replace(regExp, " ");
+  };
+
+  const getPrice = (price) => price?.toFixed(3).toString().replace(".", ",");
+
+  /*
   data
     .filter((i) => i[0])
     .map((item) => {
-      txt.push(`${item[0].toString().padStart(4, "0")}.0001="${item[1]}"`);
-      txt.push(`${item[0].toString().padStart(4, "0")}.0002="${item[2]}"`);
+      txt.push(
+        `${item[0].toString().padStart(4, "0")}.0001="${getName(item[1])}"`
+      ); //наименование
+      txt.push(
+        `${item[0].toString().padStart(4, "0")}.0002="${item[2]
+          ?.toString()
+          .replace(".", ",")}"`
+      ); //цена товара
       txt.push(`${item[0].toString().padStart(4, "0")}.0003="1"`);
-      txt.push(`${item[0].toString().padStart(4, "0")}.0004="${item[3] ?? 0}"`);
+      txt.push(
+        `${item[0].toString().padStart(4, "0")}.0004="${getCode(item[3])}"`
+      ); //штрих код
       txt.push(`${item[0].toString().padStart(4, "0")}.0005="0,000"`);
       txt.push(`${item[0].toString().padStart(4, "0")}.0006="0,000"`);
 
       return null;
     });
 
-  return ["[1]", ...txt];
+    */
+
+  txt.push("1;1;П №1;П №2;П №3;П №4;[??] Поле 5;[??] Поле 6");
+  txt.push("1;1;1;0;0;0;0;0");
+
+  data
+    .filter((i) => i[0])
+    .map((item) => {
+      txt.push(
+        `1;${item[0].toString()};"${getName(item[1])}";"${getPrice(
+          item[2]
+        )}";"1";"${getCode(item[3])}";"0,000";"0,000"`
+      );
+
+      return null;
+    });
+
+  return [...txt];
 };
